12254721edf4611fd549e7ba8ce5bde5d6c32c3f
İkram Dağcı <ikramdgc@gmail.com>
2023-08-30 16:05:20 +0300

    feat(data) : Entity relations

--- src/main/java/com/amadeus/ikramdagci/domain/entity/Airport.java
+++ src/main/java/com/amadeus/ikramdagci/domain/entity/Airport.java
@@ -1,5 +1,10 @@
 package com.amadeus.ikramdagci.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "t_airport")
 public class Airport extends BaseEntity{
 
     private String city;
--- src/main/java/com/amadeus/ikramdagci/domain/entity/Flight.java
+++ src/main/java/com/amadeus/ikramdagci/domain/entity/Flight.java
@@ -1,17 +1,20 @@
 package com.amadeus.ikramdagci.domain.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "t_flight")
 public class Flight extends BaseEntity{
 
+    @ManyToOne
     private Airport departureAirport;
+    @ManyToOne
     private Airport arrivalAirport;
     private LocalDateTime departureDateTime;
-    private LocalDateTime returnDateTime;
+    private LocalDateTime arrivalDateTime;
     private BigDecimal price;
 
 
